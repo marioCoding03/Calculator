f9ff8f52c436123c3a27abd2e3302ec040a26861
marioCoding03 <marioschprogramming@gmail.com>
2023-02-07 13:39:18 +0200

    First Commit

---
+++ main.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+std::string result;
+void check_y_Or_n(){
+	do {
+		std::cout << "Please specify 'y' or 'n': ";
+		std::cin >> result;
+	} while (result != "y" || "n");
+}
+
+void check() {
+	std::cout << "Add more numbers?" << std::endl;
+	check_y_Or_n();
+	if (result == "y") {
+		std::cout << "yes";
+	}
+	else {
+		std::cout << "no";
+	}
+}
+
+void add() {
+	double numbers = {};
+	std::cout << "enter a number: ";
+	std::cin >> numbers;
+	check();
+}
+
+int main()
+{
+	add();
+	return 0;
+}
